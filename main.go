f5f146530af77e974fae3c39ddc9cd5e526527fc
Naidu <naidu.19bci7002@vitap.ac.in>
2021-10-10 11:13:34 +0530

    final

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"go.mongodb.org/mongo-driver/bson"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -101,15 +102,14 @@ func addUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Write(response)
 }
 
-func getUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func getUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	collection, err := getMongoDbCollection(dbName, collectionUsers)
 	if err != nil {
 		panic(err)
 	}
 
 	var filter bson.M = bson.M{}
-	query := r.URL.Query()
-	t := query.Get("uid")
+	t := ps.ByName("uid")
 	if t != "" {
 		id := t
 		objID, _ := primitive.ObjectIDFromHex(id)
@@ -117,7 +117,9 @@ func getUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	}
 
 	var results []bson.M
-	cur, err := collection.Find(context.Background(), filter)
+	projection := bson.D{{"password", 0}}
+
+	cur, err := collection.Find(context.Background(), filter, options.Find().SetProjection(projection))
 	defer cur.Close(context.Background())
 
 	if err != nil {
@@ -142,7 +144,7 @@ func addPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	var post Post
 	json.NewDecoder(r.Body).Decode(&post)
-
+	post.PostedAt = time.Now().Local()
 	res, err := collection.InsertOne(context.Background(), post)
 	if err != nil {
 		panic(err)
@@ -159,21 +161,76 @@ func addPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Write(response)
 }
 
-func getPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func getPost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	collection, err := getMongoDbCollection(dbName, collectionPosts)
+	if err != nil {
+		panic(err)
+	}
 
+	var filter bson.M = bson.M{}
+	t := ps.ByName("pid")
+	if t != "" {
+		id := t
+		objID, _ := primitive.ObjectIDFromHex(id)
+		filter = bson.M{"_id": objID}
+	}
+
+	var results []bson.M
+
+	cur, err := collection.Find(context.Background(), filter)
+	defer cur.Close(context.Background())
+
+	if err != nil {
+		panic(err)
+
+	}
+
+	cur.All(context.Background(), &results)
+
+	json, _ := json.Marshal(results)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(json)
 }
 
-func getUserPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func getUserPost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	collection, err := getMongoDbCollection(dbName, collectionPosts)
+	if err != nil {
+		panic(err)
+	}
 
+	var filter bson.M = bson.M{}
+	t := ps.ByName("uid")
+	if t != "" {
+		id := t
+		//objID, _ := primitive.ObjectIDFromHex(id)
+		filter = bson.M{"user_id": id}
+	}
+
+	var results []bson.M
+	cur, err := collection.Find(context.Background(), filter)
+	defer cur.Close(context.Background())
+
+	if err != nil {
+		panic(err)
+
+	}
+
+	cur.All(context.Background(), &results)
+
+	json, _ := json.Marshal(results)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(json)
 }
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.POST("/users", addUser)
-	router.GET("/users/:uid?", getUser)
+	router.GET("/users/:uid", getUser)
 	router.POST("/posts", addPost)
-	router.GET("/posts/:pid?", getPost)
-	router.GET("/post/users/:uid?", getUserPost)
+	router.GET("/posts/:pid", getPost)
+	router.GET("/post/users/:uid", getUserPost)
 
 	log.Fatal(http.ListenAndServe("localhost:8081", router))
 }
